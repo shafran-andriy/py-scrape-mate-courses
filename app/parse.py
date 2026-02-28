bc5815dc9fc513452e31111fb2fe79785ce76f7e
shafran <shafran.andriy@gmail.com>
2026-02-28 19:04:14 +0200

    Done

--- app/parse.py
+++ app/parse.py
@@ -1,5 +1,10 @@
 from dataclasses import dataclass
+import logging
+from bs4 import BeautifulSoup
+import requests
 
+
+BASE_URL = "https://mate.academy/"
 
 @dataclass
 class Course:
@@ -9,4 +14,27 @@ class Course:
 
 
 def get_all_courses() -> list[Course]:
-    pass
+    response = requests.get(BASE_URL)
+    response.raise_for_status()
+
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    courses = []
+
+    course_cards = soup.select(".ProfessionCard_content__mPiVi")
+
+    for card in course_cards:
+        name = card.select_one(".ProfessionCard_title__m7uno")
+        description = card.select_one(".ProfessionCard_description__K8weo")
+        duration = card.select_one(".ProfessionCard_duration__13PwX")
+
+        if name and description and duration:
+            courses.append(
+                Course(
+                    name=name.get_text(strip=True),
+                    short_description=description.get_text(strip=True),
+                    duration=duration.get_text(strip=True),
+                )
+            )
+
+    return courses
